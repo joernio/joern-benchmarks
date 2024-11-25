adaa495f3ba040b968f064c2bc23b72e53cbc92e
David Baker Effendi <dave@whirlylabs.com>
2024-11-25 20:35:57 +0200

    Added some semantics for JS and tried to improve semgrep js rules

--- src/main/scala/io/joern/benchmarks/cpggen/JsSrcCpgCreator.scala
+++ src/main/scala/io/joern/benchmarks/cpggen/JsSrcCpgCreator.scala
@@ -5,7 +5,7 @@ import io.joern.benchmarks.*
 import io.joern.benchmarks.passes.{FindingsPass, JavaScriptTaggingPass}
 import io.joern.benchmarks.runner.{BenchmarkSourcesAndSinks, DefaultBenchmarkSourcesAndSinks}
 import io.joern.dataflowengineoss.layers.dataflows.{OssDataFlow, OssDataFlowOptions}
-import io.joern.dataflowengineoss.semanticsloader.{FlowMapping, FlowSemantic}
+import io.joern.dataflowengineoss.semanticsloader.{FlowMapping, FlowSemantic, NoSemantics, PassThroughMapping}
 import io.joern.jssrc2cpg.{JsSrc2Cpg, Config as JsSrcConfig}
 import io.joern.x2cpg.X2CpgFrontend
 import io.joern.x2cpg.frontendspecific.jssrc2cpg
@@ -19,6 +19,28 @@ sealed trait JavaScriptCpgCreator[Frontend <: X2CpgFrontend[?]] extends CpgCreat
 
   override def extraSemantics: List[FlowSemantic] = List(
     FlowSemantic("__ecma.String:trim", FlowMapping(0, 0) :: FlowMapping(0, -1) :: Nil),
+    FlowSemantic("__ecma.String:concat", PassThroughMapping :: FlowMapping(0, -1) :: FlowMapping(1, -1) :: Nil),
+    FlowSemantic("__ecma.Array:push", FlowMapping(0, 0) :: FlowMapping(1, 0) :: FlowMapping(1, 1) :: Nil),
+    FlowSemantic(".*.Array.*:push", FlowMapping(0, 0) :: FlowMapping(1, 0) :: FlowMapping(1, 1) :: Nil, regex = true),
+    FlowSemantic("__ecma\\..*:toLowerCase", FlowMapping(0, 0) :: FlowMapping(0, -1) :: Nil, regex = true),
+    FlowSemantic(
+      "__ecma.String:slice",
+      FlowMapping(0, 0) :: FlowMapping(0, -1) :: FlowMapping(1, 1) :: FlowMapping(2, 2) :: Nil
+    ),
+    FlowSemantic("fs:writeFileSync", FlowMapping(0, 0) :: FlowMapping(1, 1) :: FlowMapping(1, -1) :: Nil),
+    FlowSemantic("fs:createWriteStream", FlowMapping(0, 0) :: FlowMapping(1, 1) :: FlowMapping(1, -1) :: Nil),
+    FlowSemantic("fs:createReadStream", FlowMapping(0, 0) :: FlowMapping(1, 1) :: FlowMapping(1, -1) :: Nil),
+    FlowSemantic("fs:open", FlowMapping(0, 0) :: FlowMapping(1, 1) :: FlowMapping(1, -1) :: Nil),
+    FlowSemantic(
+      "path:join",
+      FlowMapping(0, 0) :: FlowMapping(0, -1) :: FlowMapping(1, 1) :: FlowMapping(2, 2) :: FlowMapping(
+        1,
+        -1
+      ) :: FlowMapping(2, -1) :: Nil
+    ),
+    FlowSemantic(".*:query", PassThroughMapping :: Nil),
+    FlowSemantic("encodeURI", Nil),
+    FlowSemantic("encodeURIComponent", Nil),
     FlowSemantic(Operators.logicalNot, FlowMapping(1, -1) :: FlowMapping(2, -1) :: Nil),
     FlowSemantic(Operators.logicalAnd, FlowMapping(1, -1) :: FlowMapping(2, -1) :: Nil)
   )
